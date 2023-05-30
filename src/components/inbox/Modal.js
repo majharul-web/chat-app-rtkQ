108522e6759fa4728162c30c6306668ae67d2f4c
majharul <majharuljony163@gmail.com>
2023-05-30 17:03:06 +0600

    send and edtie with modal

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,9 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  conversationsApi,
+  useAddConversationMutation,
+  useEditConversationMutation,
+} from "../../features/conversations/conversationsApi";
+import { useGetUserQuery } from "../../features/users/usersApi";
 import isValidEmail from "../../utils/isValidEmail";
+import Error from "../ui/Error";
 
 export default function Modal({ open, control }) {
   const [to, setTo] = useState("");
   const [message, setMessage] = useState("");
+  const [userCheck, setUserCheck] = useState(false);
+  const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+  const { email: myEmail } = loggedInUser || {};
+  const dispatch = useDispatch();
+  const [responseError, setResponseError] = useState("");
+  const [conversation, setConversation] = useState(undefined);
+
+  const { data: participant } = useGetUserQuery(to, {
+    skip: !userCheck,
+  });
+
+  const [addConversation, { isSuccess: isAddConversationSuccess }] = useAddConversationMutation();
+  const [editConversation, { isSuccess: isEditConversationSuccess }] = useEditConversationMutation();
+
+  useEffect(() => {
+    if (participant?.length > 0 && participant[0].email !== myEmail) {
+      // check conversation existance
+      dispatch(
+        conversationsApi.endpoints.getConversation.initiate({
+          userEmail: myEmail,
+          participantEmail: to,
+        })
+      )
+        .unwrap()
+        .then((data) => {
+          setConversation(data);
+        })
+        .catch((err) => {
+          setResponseError("There was a problem!");
+        });
+    }
+  }, [participant, dispatch, myEmail, to]);
+
+  // listen conversation add/edit success
+  useEffect(() => {
+    if (isAddConversationSuccess || isEditConversationSuccess) {
+      control();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAddConversationSuccess, isEditConversationSuccess]);
 
   const debounceHandler = (fn, delay) => {
     let timeoutId;
@@ -17,13 +65,38 @@ export default function Modal({ open, control }) {
 
   const doSearch = (value) => {
     if (isValidEmail(value)) {
-      console.log("valid email", value);
       // check user API
+      setUserCheck(true);
       setTo(value);
     }
   };
 
   const handleSearch = debounceHandler(doSearch, 500);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (conversation?.length > 0) {
+      // edit conversation
+      editConversation({
+        id: conversation[0].id,
+        data: {
+          participants: `${myEmail}-${participant[0].email}`,
+          users: [loggedInUser, participant[0]],
+          message,
+          timestamp: new Date().getTime(),
+        },
+      });
+    } else if (conversation?.length === 0) {
+      // add conversation
+      addConversation({
+        participants: `${myEmail}-${participant[0].email}`,
+        users: [loggedInUser, participant[0]],
+        message,
+        timestamp: new Date().getTime(),
+      });
+    }
+  };
 
   return (
     open && (
@@ -31,7 +104,7 @@ export default function Modal({ open, control }) {
         <div onClick={control} className='fixed w-full h-full inset-0 z-10 bg-black/50 cursor-pointer'></div>
         <div className='rounded w-[400px] lg:w-[600px] space-y-8 bg-white p-10 absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2'>
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Send message</h2>
-          <form className='mt-8 space-y-6'>
+          <form className='mt-8 space-y-6' onSubmit={handleSubmit}>
             <div className='rounded-md shadow-sm -space-y-px'>
               <div>
                 <label htmlFor='to' className='sr-only'>
@@ -68,12 +141,20 @@ export default function Modal({ open, control }) {
               <button
                 type='submit'
                 className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
+                disabled={
+                  conversation === undefined || (participant?.length > 0 && participant[0].email === myEmail)
+                }
               >
                 Send Message
               </button>
             </div>
 
-            {/* <Error message="There was an error" /> */}
+            {participant?.length === 0 && <Error message='This user does not exist!' />}
+            {participant?.length > 0 && participant[0].email === myEmail && (
+              <Error message='You can not send message to yourself!' />
+            )}
+
+            {responseError && <Error message={responseError} />}
           </form>
         </div>
       </>
