5f92e30b168475ba4f0af0c76d89c62cdb97fbb9
majharul <majharuljony163@gmail.com>
2023-05-30 17:20:17 +0600

    modal messages function done

--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -4,6 +4,7 @@ import Message from "./Message";
 export default function Messages({ messages = [] }) {
   const { user } = useSelector((state) => state.auth) || {};
   const { email } = user || {};
+
   return (
     <div className='relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse'>
       <ul className='space-y-2'>
@@ -11,9 +12,11 @@ export default function Messages({ messages = [] }) {
           .slice()
           .sort((a, b) => a.timestamp - b.timestamp)
           .map((message) => {
-            const { id, message: latMessage, sender } = message || {};
-            const justify = sender.email === email ? "end" : "start";
-            return <Message justify={justify} message='I am fine what about you?' />;
+            const { message: lastMessage, id, sender } = message || {};
+
+            const justify = sender.email !== email ? "start" : "end";
+
+            return <Message key={id} justify={justify} message={lastMessage} />;
           })}
       </ul>
     </div>
